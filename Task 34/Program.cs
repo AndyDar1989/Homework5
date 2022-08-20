5d23e11d8f991f585347d5c4feb7a6b0491804e4
AndrewDar <geologalone@gmail.com>
2022-08-20 16:44:45 +0300

    programs for hometask created

---
+++ Task 34/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+int EvenNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+
+    return count;
+}
+
+
+int[] arr = CreateArrayRndInt(4, 100, 1000);
+PrintArr(arr);
+int result = EvenNum(arr);
+Console.WriteLine();
+Console.WriteLine($"The number of even numbers in the array is {result}");
---
+++ Task 38/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 38: Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементами массива.
+[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2*/
+
+
+double[] CreateArrayRndDouble(int size, double min, double max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return array;
+}
+
+void PrintArr(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(Math.Round(array[i], 2) + "  ");
+    }
+    Console.Write(Math.Round(array[array.Length - 1], 2));
+    Console.Write("]");
+}
+
+double MinMaxDiff(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    double diff = max - min; return diff;
+}
+
+double[] arr = CreateArrayRndDouble(5, -10.0, 10.0);
+PrintArr(arr);
+double result = MinMaxDiff(arr);
+Console.WriteLine();
+Console.WriteLine($"The difference between maximal and minimal elements of the array is {Math.Round(result, 2)}");
